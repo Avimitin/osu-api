f249d4436840d747b959c3479e2793ad1e55b969
Avimitin <avimitin@gmail.com>
2023-01-09 19:18:06 +0800

    Implement GetBeatmapsProps
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ src/api_v1/models/beatmaps.rs
@@ -0,0 +1,99 @@
+use typed_builder::TypedBuilder;
+
+use super::{Mods, OsuMode};
+
+#[derive(Debug)]
+pub enum UserId {
+  Id(u64),
+  Username(String),
+}
+
+impl From<u64> for UserId {
+  fn from(id: u64) -> Self {
+    Self::Id(id)
+  }
+}
+
+impl From<String> for UserId {
+  fn from(name: String) -> Self {
+    Self::Username(name)
+  }
+}
+
+// TODO: Add `since`
+#[derive(Debug, TypedBuilder)]
+#[builder(builder_type_doc = "Builder for creating request to get_beatmaps API")]
+pub struct GetBeatmapsProps {
+  #[builder(default = 0)]
+  beatmapset_id: u64,
+  #[builder(default = 0)]
+  beatmap_id: u64,
+  #[builder(setter(transform = |id: impl Into<UserId>| id.into()))]
+  user_id: UserId,
+  #[builder(default, setter(strip_option))]
+  mode: Option<OsuMode>,
+  #[builder(setter(strip_bool))]
+  include_converted: bool,
+  #[builder(default, setter(strip_option))]
+  beatmap_hash: Option<String>,
+  #[builder(default = 0)]
+  limit: u32,
+  #[builder(default = Vec::new())]
+  mods: Vec<Mods>,
+}
+
+impl GetBeatmapsProps {
+  pub(crate) fn into_query_param(self, key: &str) -> Vec<(String, String)> {
+    let mut query: Vec<(String, String)> = Vec::with_capacity(9);
+
+    query.push(("k".to_string(), key.to_string()));
+
+    if self.beatmapset_id == 0 && self.beatmap_id == 0 {
+      // TODO: return error here
+    }
+
+    if self.beatmapset_id != 0 {
+      query.push(("s".to_string(), self.beatmapset_id.to_string()))
+    }
+
+    match self.user_id {
+      UserId::Id(id) => {
+        query.push(("u".to_string(), id.to_string()));
+        query.push(("type".to_string(), "id".to_string()));
+      }
+      UserId::Username(name) => {
+        query.push(("u".to_string(), name));
+        query.push(("type".to_string(), "string".to_string()));
+      }
+    };
+
+    if let Some(mode) = self.mode {
+      query.push(("m".to_string(), mode.to_string()));
+
+      let include_converted = if self.include_converted { "1" } else { "0" };
+      match mode {
+        OsuMode::Standard => (), // do nothing
+        _ => query.push(("a".to_string(), include_converted.to_string())),
+      }
+    }
+
+    if let Some(hash) = self.beatmap_hash {
+      query.push(("h".to_string(), hash))
+    }
+
+    if self.limit != 0 {
+      query.push(("limit".to_string(), self.limit.to_string()))
+    }
+
+    if !self.mods.is_empty() {
+      let mods = self.mods.into_iter().fold(0_u64, |accum, item| {
+        let bit: u64 = item.into();
+        accum | bit
+      });
+
+      query.push(("mods".to_string(), mods.to_string()))
+    }
+
+    query
+  }
+}
---
+++ src/api_v1/models/mod.rs
@@ -0,0 +1,114 @@
+mod beatmaps;
+
+pub use beatmaps::GetBeatmapsProps;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum OsuMode {
+  Standard,
+  Taiko,
+  CatchTheBeat,
+  Mania,
+}
+
+impl ToString for OsuMode {
+  fn to_string(&self) -> String {
+    self.as_ref().to_string()
+  }
+}
+
+impl AsRef<str> for OsuMode {
+  fn as_ref(&self) -> &str {
+    use OsuMode::*;
+
+    match self {
+      Standard => "0",
+      Taiko => "1",
+      CatchTheBeat => "2",
+      Mania => "3",
+    }
+  }
+}
+
+#[derive(Debug)]
+pub enum Mods {
+  None,
+  NoFail,
+  Easy,
+  TouchDevice,
+  Hidden,
+  HardRock,
+  SuddenDeath,
+  DoubleTime,
+  Relax,
+  HalfTime,
+  Nightcore, // Only set along with DoubleTime. i.e: NC only gives 576
+  Flashlight,
+  Autoplay,
+  SpunOut,
+  Relax2,  // Autopilot
+  Perfect, // Only set along with SuddenDeath. i.e: PF only gives 16416
+  Key4,
+  Key5,
+  Key6,
+  Key7,
+  Key8,
+  FadeIn,
+  Random,
+  Cinema,
+  Target,
+  Key9,
+  KeyCoop,
+  Key1,
+  Key3,
+  Key2,
+  ScoreV2,
+  Mirror,
+}
+
+impl From<Mods> for u64 {
+  fn from(mods: Mods) -> Self {
+    // special case
+    if let Mods::None = mods {
+      return 0;
+    }
+
+    let ret = 1 << ((mods as u64) - 1);
+
+    match ret {
+      // NC should be set along with DT
+      512 => {
+        let mods = Mods::DoubleTime;
+        let dt: u64 = mods.into();
+        ret | dt
+      }
+      // PF should be set along with SD
+      16384 => {
+        let mods = Mods::SuddenDeath;
+        let sd: u64 = mods.into();
+        ret | sd
+      }
+      _ => ret,
+    }
+  }
+}
+
+#[test]
+fn test_mods_bit() {
+  let eq = |get: u64, expect: u64| {
+    assert_eq!(get, expect);
+  };
+
+  let mods = Mods::HardRock;
+  eq(mods.into(), 16);
+
+  let mods = Mods::Nightcore;
+  eq(mods.into(), 576);
+
+  let mods = Mods::Perfect;
+  eq(mods.into(), 16416);
+
+  let mods = Mods::Key2;
+  eq(mods.into(), 268435456);
+}
