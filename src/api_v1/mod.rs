7597e9db3c5351d6778141ba6c5897570bf46358
Avimitin <avimitin@gmail.com>
2023-01-20 22:43:41 +0800

    Remove the utils module
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/api_v1/mod.rs
+++ src/api_v1/mod.rs
@@ -1,9 +1,9 @@
 mod api;
 mod models;
 mod req;
-mod utils;
 
 pub use api::{ApiEndpoint, Error, OsuApiRequester};
 pub use models::{
-  GetBeatmapsProps, GetBeatmapsResp, GetUserRecentProp, GetUserRecentResp, ModsFlag,
+  GameMode, GetBeatmapsProps, GetBeatmapsResp, GetUserRecentProp, GetUserRecentResp, ModsFlag,
+  UserId,
 };
