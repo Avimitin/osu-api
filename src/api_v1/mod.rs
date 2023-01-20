9dbc0f00ea3f0066272efb3274d1b43fd60b7d2a
Avimitin <avimitin@gmail.com>
2023-01-20 21:33:44 +0800

    Implement get_beatmaps
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/api_v1/mod.rs
+++ src/api_v1/mod.rs
@@ -9,6 +9,11 @@ pub trait OsuApiRequester {
     &self,
     param: models::GetUserRecentProp<'k, 'u>,
   ) -> Result<Vec<models::GetUserRecentResp>, Error>;
+
+  async fn get_beatmaps<'u, 'k>(
+    &self,
+    param: models::GetBeatmapsProps<'u, 'k>,
+  ) -> Result<Vec<models::GetBeatmapsResp>, Error>;
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -54,6 +59,19 @@ impl OsuApiRequester for reqwest::Client {
 
     Ok(recent)
   }
+
+  async fn get_beatmaps<'u, 'k>(
+    &self,
+    param: models::GetBeatmapsProps<'u, 'k>,
+  ) -> Result<Vec<models::GetBeatmapsResp>, Error> {
+    let param: HashMap<&'static str, String> =
+      param.try_into().map_err(|_| Error::InvalidRequestParams)?;
+    let url = reqwest::Url::parse_with_params(API_GET_BEATMAPS, &param)
+      .expect("fail to turn given param into query");
+    let resp = self.get(url).send().await?.bytes().await?;
+    let recent: Vec<models::GetBeatmapsResp> = serde_json::from_slice(&resp)?;
+    Ok(recent)
+  }
 }
 
 #[tokio::test]
@@ -71,4 +89,42 @@ async fn test_get_user_recent() {
   let resp = client.get_user_recent(props).await.unwrap();
 
   assert!(!resp.is_empty())
+}
+
+#[tokio::test]
+async fn test_get_beatmaps() {
+  dotenvy::dotenv().ok();
+
+  let api_key = std::env::var("OSU_API_KEY").expect("Require env `OSU_API_KEY` set");
+  let client = reqwest::Client::new();
+
+  // #1 Test get beatmap set
+  let props = models::GetBeatmapsProps::builder()
+    .api_key(&api_key)
+    .beatmapset_id(896080)
+    .build();
+  let resp = client.get_beatmaps(props).await.unwrap();
+  // tsukinami have 3 difficulties
+  assert_eq!(resp.len(), 3);
+
+  // #2 Test get single difficulty
+  let props = models::GetBeatmapsProps::builder()
+    .api_key(&api_key)
+    .beatmap_id(1872396)
+    .build();
+  let resp = client.get_beatmaps(props).await.unwrap();
+  assert_eq!(resp.len(), 1);
+
+  // #3 Test mods
+  let props = models::GetBeatmapsProps::builder()
+    .api_key(&api_key)
+    .beatmap_id(1872396)
+    .mods(vec![models::ModsFlag::DOUBLETIME, models::ModsFlag::EASY])
+    .build();
+  let resp = client.get_beatmaps(props).await.unwrap();
+  assert!(!resp.is_empty());
+
+  // ensure mods is applied
+  let map = &resp[0];
+  assert!(map.difficultyrating > 6.01);
 }
